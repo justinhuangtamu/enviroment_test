8bd6ef5b15a32c63761db8639c5f88965396c936
jh0893 <jh0893@tamu.edu>
2022-09-15 20:45:30 -0500

    author price date test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -11,3 +11,32 @@ RSpec.describe 'Creating a book', type: :feature do
   end
 end
 
+RSpec.describe 'Add author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('rowling')
+  end
+end
+
+RSpec.describe 'Add price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: '7'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('7')
+  end
+end
+
+RSpec.describe 'Add date', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Date', with: '10/31/1981'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('10/31/1981')
+  end
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   subject do
     described_class.new(title: 'harry potter')
+	described_class.new(author: 'rowling')
+	described_class.new(price: '7')
+	described_class.new(date: '10/31/1981')
   end
 
   it 'is valid with valid attributes' do
@@ -12,7 +15,10 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+	subject.author = nil
+	subject.price = nil
+	subject.date = nil
     expect(subject).not_to be_valid
   end
+  
 end
-
