b54427322e783f44876dce56e968cd35c9365383
justinhuangtamu <102560775+justinhuangtamu@users.noreply.github.com>
2022-09-20 00:10:29 -0500

    Update 20220909153129_create_books.rb

--- db/migrate/20220909153129_create_books.rb
+++ db/migrate/20220909153129_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.string :title, null: false
-	  t.string :author, null: false
+      t.string :title
+	  t.string :author
 	  t.float :price
 	  t.date :date
 
