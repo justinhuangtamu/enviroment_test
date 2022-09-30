1caa25cfef3ad47ec2284253fe5247887fc227db
jh0893 <jh0893@tamu.edu>
2022-09-30 08:37:52 -0500

    scaffold reset

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-	validates :title, presence: true
+    validates :title, presence: true
 	validates :author, presence: true
 	validates :price, presence: true
 	validates :date, presence: true
